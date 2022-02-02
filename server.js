f0d8b1fbb0447a6b4cdba96f25df1ae1282a2643
Daniel Radosa <danysko5@gmail.com>
2022-02-02 08:35:29 +0100

    Added definitions, importing routes, and connection to MongoDB

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ const bodyParser = require("body-parser");
 const app = express();
 
 require("dotenv-flow").config();
+// DEFINITIONS
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// IMPORT ROUTES
+const switchRoute = require('./routes/switch');
+const authRoute = require('./routes/auth');
+
+// CONNECT TO MONGOdb
+mongoose.connect (
+    process.env.DBHOST,  { useUnifiedTopology: true, useNewUrlParser: true }
+  ).catch(error => console.log("Error connecting to MongoDB: " + error));
+  
+  mongoose.connection.once('open', () => console.log('Connected succesfully to MongoDB'));
 
 // ROUTES
 app.get("/api/welcome", (req, res) => {
